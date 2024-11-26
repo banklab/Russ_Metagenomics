6b7acd9e6647ab0516eca80b85a5c8153b2ddffa
russjasp <61029511+russjasp@users.noreply.github.com>
2024-11-26 13:07:04 +0100

    Update 01_MAKE_METASPADES_SH.R

--- 06_Assembly/01_MAKE_METASPADES_SH.R
+++ 06_Assembly/01_MAKE_METASPADES_SH.R
@@ -20,14 +20,14 @@ for(i in 1:length(sample_list)){ ## loop over each sample, write a new sh script
   ## code block for metaspades
   ## adjust MEM, cpus here as well as in slurm script
   ## choose kmer lengths here
-  code_block <- paste0("metaspades.py --pe1-1 ",INPUT_DIR,"/",samp2,".R1.dedup.fastq --pe1-2 ",INPUT_DIR,"/",samp2,".R2.dedup.fastq --meta -o ",OUTPUT_DIR,"/",samp2,"_deer.asm -t 60 -m 195 -k 21,33,55,77")
+  code_block <- paste0("metaspades.py --pe1-1 ",INPUT_DIR,"/",samp2,".R1.dedup.fastq --pe1-2 ",INPUT_DIR,"/",samp2,".R2.dedup.fastq --meta -o ",OUTPUT_DIR,"/",samp2,"_deer.asm -t 16 -m 195 -k 21,33,55,77")
   
   write ("#!/bin/bash", sh_name)
   write ("#SBATCH --mem=200000M", sh_name, append = TRUE) ## adjust MEM, time, cpus etc
   write ("#SBATCH --nodes=1", sh_name, append = TRUE)
   write ("#SBATCH --ntasks=1", sh_name, append = TRUE)
-  write ("#SBATCH --cpus-per-task=60", sh_name, append = TRUE)
-  write ("#SBATCH --time=04:00:00", sh_name, append = TRUE)
+  write ("#SBATCH --cpus-per-task=16", sh_name, append = TRUE)
+  write ("#SBATCH --time=24:00:00", sh_name, append = TRUE)
   write ("#SBATCH --mail-user=<russell.jasper@unibe.ch>", sh_name, append = TRUE) ## adjust email for job finish/fail etc
   write ("#SBATCH --mail-type=FAIL,END", sh_name, append = TRUE)
   write ("#SBATCH --output=slurm-%x.%j.out", sh_name, append = TRUE)
