38d613094c84055e59cd13316133d5502c46702a
russjasp <61029511+russjasp@users.noreply.github.com>
2024-11-26 10:27:01 +0100

    Update 01_edit_headers_and_rename_fasta.sh

--- 11_dRep/01_edit_headers_and_rename_fasta.sh
+++ 11_dRep/01_edit_headers_and_rename_fasta.sh
@@ -1,13 +1,15 @@
 
-## BIN REFINER
-DATASET=RAS
+## for each fasta in each sample, add the sample id and bin id to fasta header
+## I do this to keep track of where each fasta came from
+
+DATASET=deer
 
 INDIR=/storage/scratch/users/rj23k073/01_RAS/10_Consolidate_Bins/05_FINAL_BINS
-OUTDIR=/storage/scratch/users/rj23k073/01_RAS/11_dRep/01_Bin_Ref
+OUTDIR=/storage/scratch/users/rj23k073/04_DEER/11_dRep
 
 cd $INDIR
 
-for i in *RAS
+for i in *deer
  do
  cd "$i"
  for j in *fa
@@ -18,27 +20,4 @@ for i in *RAS
   done
  cd $INDIR
 done
-
-
-## DASTOOLS
-DATASET=deer
-
-INDIR=/storage/scratch/users/rj23k073/04_DEER/10_DAS_Tool
-OUTDIR=/storage/scratch/users/rj23k073/04_DEER/11_dRep/01_Pre_Bins
-
-cd $INDIR
-
-for i in *deer
- do
- cd "$i"/*_DASTool_bins
- for j in *fa
-  do
-  header2=asm_$(echo "$i" | perl -pe 's/_"$DATASET"//')_$(echo "$j" | perl -pe 's/.fa//')
-  rename="$i"_"$j"
-  sed "s/>.*/&_$header2/" $j > "$OUTDIR"/"$rename"
-  done
- cd $INDIR
-done
-
-
 
