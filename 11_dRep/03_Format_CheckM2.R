89e5218d344cc8273682c7bb88761bc43e6e2ce6
russjasp <61029511+russjasp@users.noreply.github.com>
2024-11-27 12:24:37 +0100

    Update 03_Format_CheckM2.R

--- 11_dRep/03_Format_CheckM2.R
+++ 11_dRep/03_Format_CheckM2.R
@@ -4,4 +4,6 @@ checkm <- read.table("quality_report.tsv", header=T, stringsAsFactors=F, sep="\t
 
 colnames(checkm)[1:3] <- c("genome","completeness","contamination")
 
+checkm$genome <- paste0(checkm$genome,".fa")
+
 write.csv(checkm, "genomeInformation.csv", row.names=F)
