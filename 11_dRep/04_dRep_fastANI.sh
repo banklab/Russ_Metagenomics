2acc3ee43575df88577c0869dd7dc395417a07c3
russjasp <61029511+russjasp@users.noreply.github.com>
2024-11-27 08:51:07 +0100

    Update 04_dRep_fastANI.sh

--- 11_dRep/04_dRep_fastANI.sh
+++ 11_dRep/04_dRep_fastANI.sh
@@ -1,6 +1,3 @@
-
-**fastANI algorithm = conda activate fastani
-
 #!/bin/bash
 #SBATCH --mem=20000M
 #SBATCH --time=02:00:00
@@ -10,8 +7,4 @@
 #SBATCH --mail-user=<russell.jasper@unibe.ch>
 #SBATCH --mail-type=FAIL,END
 #SBATCH --output=slurm-%x.%j.out
-module load Python/3.10.8-GCCcore-12.2.0
-module load vital-it/7
-module load SequenceAnalysis/GenePrediction/prodigal/2.6.3
-module load UHTS/Analysis/Mash/2.0
-dRep dereplicate dRep_ONLY_bin_ref_fastANI -g 01_Bin_Ref_Bins/*fa -comp 70 -con 10 --S_algorithm fastANI --gen_warnings -ms 10000 -pa 0.8 -sa 0.95 -nc 0.30 --genomeInfo genomeInformation_ASM_bin_ref.csv -p 16 --run_tertiary_clustering -sizeW 1
+dRep dereplicate 03_drep_bins -g 01_input_bins/*fa -comp 70 -con 10 --S_algorithm fastANI --gen_warnings -ms 10000 -pa 0.8 -sa 0.95 -nc 0.30 --genomeInfo genomeInformation.csv -p 16 --run_tertiary_clustering -sizeW 1
