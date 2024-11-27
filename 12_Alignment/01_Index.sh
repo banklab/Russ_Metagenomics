fec9d9f364f178494316d304909c5176f5488165
russjasp <61029511+russjasp@users.noreply.github.com>
2024-11-27 14:39:47 +0100

    Update 01_Index.sh

--- 12_Alignment/01_Index.sh
+++ 12_Alignment/01_Index.sh
@@ -3,11 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mail-user=<russell.jasper@unibe.ch>
 #SBATCH --mail-type=FAIL,END
 #SBATCH --output=slurm-%x.%j.out
-module load vital-it/7
-module load UHTS/Aligner/bowtie2
-module load UHTS/Analysis/samtools/1.10
-bowtie2-build dRep_ONLY_bin_ref_fastANI.fasta dRep_ONLY_bin_ref_fastANI
+module load Bowtie2/2.4.4-GCC-10.3.0
+bowtie2-build NoNorm_deer.fa NoNorm_deer
