874700beed1b40ee1c9676aa16522397d40760d2
russjasp <61029511+russjasp@users.noreply.github.com>
2024-11-29 12:30:21 +0100

    Update 04_Align_bwa_mem.sh

--- x_AllTheBacteria/04_Align_bwa_mem.sh
+++ x_AllTheBacteria/04_Align_bwa_mem.sh
@@ -8,12 +8,15 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --output=slurm-%x.%j.out
 module load BWA/0.7.17-GCC-10.3.0
+module load SAMtools/1.13-GCC-10.3.0
 for i in $(cat dir_list_all.txt)
 do
   cd $i
   for j in {1..10}
   do
     bwa mem "$i"_database.fa /storage/scratch/users/rj23k073/04_DEER/04_FastUniq/1_"$j".R1.dedup.fastq /storage/scratch/users/rj23k073/04_DEER/04_FastUniq/1_"$j".R2.dedup.fastq > deer_1_"$j"_"$i".sam
+    samtools view -Sb deer_1_"$j"_"$i".sam > deer_1_"$j"_"$i".bam
+    rm deer_1_"$j"_"$i".sam
   done
   echo "done:" $i
   cd ..
