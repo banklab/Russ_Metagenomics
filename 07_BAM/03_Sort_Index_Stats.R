22a5601cf7d845f6c2f1542063851791a21e13c9
russjasp <61029511+russjasp@users.noreply.github.com>
2024-11-29 11:24:29 +0100

    Update 03_Sort_Index_Stats.R

--- 07_BAM/03_Sort_Index_Stats.R
+++ 07_BAM/03_Sort_Index_Stats.R
@@ -17,7 +17,7 @@ for(i in 1:length(Read_list)){
   write ("#SBATCH --nodes=1", sh_name, append = TRUE)
   write ("#SBATCH --ntasks=1", sh_name, append = TRUE)
   write ("#SBATCH --cpus-per-task=1", sh_name, append = TRUE)
-  write ("#SBATCH --time=12:00:00", sh_name, append = TRUE)
+  write ("#SBATCH --time=6:00:00", sh_name, append = TRUE)
   write ("#SBATCH --mail-user=<russell.jasper@unibe.ch>", sh_name, append = TRUE)
   write ("#SBATCH --mail-type=FAIL,END", sh_name, append = TRUE)
   write ("#SBATCH --output=slurm-%x.%j.out", sh_name, append = TRUE)
