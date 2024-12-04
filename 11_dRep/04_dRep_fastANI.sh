964a9f9b7dfffddca215744c764d3f47e3db7365
russjasp <61029511+russjasp@users.noreply.github.com>
2024-12-04 12:08:59 +0100

    Update 04_dRep_fastANI.sh

--- 11_dRep/04_dRep_fastANI.sh
+++ 11_dRep/04_dRep_fastANI.sh
@@ -8,4 +8,4 @@
 #SBATCH --mail-type=FAIL,END
 #SBATCH --output=slurm-%x.%j.out
 #SBATCH --partition=pibu_el8
-dRep dereplicate 03_drep_bins -g 01_input_bins/*fa -comp 70 -con 10 --S_algorithm fastANI --gen_warnings -ms 10000 -pa 0.8 -sa 0.95 -nc 0.30 --genomeInfo genomeInformation.csv -p 16
+dRep dereplicate 03_drep_bins -g 01_input_bins/*fa -comp 70 -con 10 --S_algorithm ANImf --gen_warnings -ms 10000 -pa 0.8 -sa 0.95 -nc 0.30 --genomeInfo genomeInformation.csv -p 16
