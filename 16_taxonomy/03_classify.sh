5ab486a61b2364269449783bbac68b99955496d1
russjasp <61029511+russjasp@users.noreply.github.com>
2024-12-06 11:31:11 +0100

    Update 03_classify.sh

--- 16_taxonomy/03_classify.sh
+++ 16_taxonomy/03_classify.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --mem=120000M
-#SBATCH --time=24:00:00
+#SBATCH --time=4:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=10
 #SBATCH --mail-user=<russell.jasper@unibe.ch>
 #SBATCH --mail-type=FAIL,END
 #SBATCH --output=slurm-%x.%j.out
@@ -13,5 +13,5 @@ module load prodigal/2.6.3-GCCcore-10.3.0
 module load HMMER/3.3.2-gompi-2021a
 export GTDBTK_DATA_PATH=/storage/workspaces/vetsuisse_fiwi_mico4sys/fiwi_mico4sys001/metagenomics/programs/GTDB/release220/
 
-gtdbtk classify --genome_dir /storage/scratch/users/rj23k073/04_DEER/REFERENCES/genomes --align_dir . --out_dir . -x fa --cpus 1 --mash_db mash_db
+gtdbtk classify --genome_dir /storage/scratch/users/rj23k073/04_DEER/REFERENCES/genomes --align_dir . --out_dir . -x fa --cpus 10 --mash_db mash_db
 
