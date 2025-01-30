fa9d767f506653e78f0ca2e10e7991b07abc2362
russjasp <61029511+russjasp@users.noreply.github.com>
2025-01-30 09:48:43 +0100

    Create fimo.sh

---
+++ 20_MEME_Suite/fimo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --mem=1000M
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:40:00
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+fimo --oc collect /storage/scratch/users/rj23k073/04_DEER/20_Meme/motif_databases/PROKARYOTE/collectf.meme *fa
+fimo --oc fan /storage/scratch/users/rj23k073/04_DEER/20_Meme/motif_databases/PROKARYOTE/fan2020.meme *fa
+fimo --oc prodoric /storage/scratch/users/rj23k073/04_DEER/20_Meme/motif_databases/PROKARYOTE/prodoric_2021.9.meme *fa
+fimo --oc regtransbase /storage/scratch/users/rj23k073/04_DEER/20_Meme/motif_databases/PROKARYOTE/regtransbase.meme *fa
