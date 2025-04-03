d3dac173f0cb977a7697e3d3ed2736af07fb8207
russjasp <61029511+russjasp@users.noreply.github.com>
2025-04-03 09:39:25 +0200

    Update 04_format_taxonomy.R

--- 16_taxonomy/04_format_taxonomy.R
+++ 16_taxonomy/04_format_taxonomy.R
@@ -7,7 +7,7 @@ setwd("/Users/russjasper/Dropbox/My Mac (Russs-MacBook-Air.local)/Desktop/BERN/R
 DF <- data.frame(fread("gtdbtk.bac120.summary.tsv", header=T, stringsAsFactors = F, sep="\t"))
 DF2 <- data.frame(fread("gtdbtk.ar53.summary.tsv", header=T, stringsAsFactors = F, sep="\t"))
 
-DF$bin <- gsub("_deer","",DF$user_genome)
+DF$bin <- gsub("_deer","",DF$user_genome) ## extract taxonomy information for Bacteria
 
 DF$Domain <- gsub("d__|;p__.*","",DF$classification)
 DF$Phylum <- gsub(".*p__|;c__.*","",DF$classification)
@@ -18,7 +18,7 @@ DF$Genus <- gsub(".*g__|;s__.*","",DF$classification)
 DF$Species <- gsub(".*s__","",DF$classification)
 
 
-DF2$bin <- gsub("_deer","",DF2$user_genome)
+DF2$bin <- gsub("_deer","",DF2$user_genome) ## extract taxonomy information for Archaea
 
 DF2$Domain <- gsub("d__|;p__.*","",DF2$classification)
 DF2$Phylum <- gsub(".*p__|;c__.*","",DF2$classification)
