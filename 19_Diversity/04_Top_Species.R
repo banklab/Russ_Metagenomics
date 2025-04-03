e9e0b8857b1d97b18080c2eaf0d8d414c5f426a2
russjasp <61029511+russjasp@users.noreply.github.com>
2025-04-03 15:48:36 +0200

    Update 04_Top_Species.R

--- 19_Diversity/04_Top_Species.R
+++ 19_Diversity/04_Top_Species.R
@@ -1,1 +1,9 @@
 
+
+setwd("/Users/russjasper/Dropbox/My Mac (Russs-MacBook-Air.local)/Desktop/BERN/RESULTS2/DEER/19_Diversity")
+outlier_df <- read.csv("outlier_df.csv", header=T, stringsAsFactors = F)
+
+top_species <- unique(outlier_df$bin)
+
+
+
