8a7438f82ca16d2d02f99ab9783599b0ecae7fb6
russjasp <61029511+russjasp@users.noreply.github.com>
2025-04-03 13:02:54 +0200

    Update 02_read_eggnog_into_R.R

--- 18_eggNOG/02_read_eggnog_into_R.R
+++ 18_eggNOG/02_read_eggnog_into_R.R
@@ -1,6 +1,7 @@
 
 library(readr)
 
+## read eggNOG output into R
 read_eggnog <- function (file){
   col_names <- c("query_name", "seed_eggNOG_ortholog", "seed_ortholog_evalue", 
                  "seed_ortholog_score", "predicted_gene_name", "GO_terms", "KEGG_KOs", 
@@ -24,7 +25,7 @@ for(i in 1:length(eggs)){
 }
 
 setwd("/rs_scratch/users/rj23k073/04_DEER/18_eggNOG")
-
+## general formatting etc
 colnames(total_egg) <- c('query_name','seed_eggNOG_ortholog','seed_ortholog_evalue','seed_ortholog_score','best_tax_level','Preferred_name','GOs','EC','KEGG_ko','KEGG_Pathway','KEGG_Module','KEGG_Reaction','KEGG_rclass','BRITE','KEGG_TC','CAZy','BiGG_Reaction','taxonomic_scope','eggNOG_OGs','best_eggNOG_OG','COG_Functional_cat.','eggNOG_free_text_desc.')
 
 total_egg$bin <- sub("_(?!.*_).*", "", gsub(".*asm_","",total_egg$query_name), perl=T)
