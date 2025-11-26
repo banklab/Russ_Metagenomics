b545dbdebb6b8b8576ed6baf650e85cb5fbe6067
russjasp <61029511+russjasp@users.noreply.github.com>
2025-11-26 10:11:59 +0100

    Update 02_HUMAnN.sh

--- 30_Community/02_HUMAnN.sh
+++ 30_Community/02_HUMAnN.sh
@@ -1,8 +1,16 @@
 
 conda activate human
 
-humann --input sample_R1.fastq.gz,sample_R2.fastq.gz \
-      --output humann_out/sample \
+
+INDIR=/storage/workspaces/vetsuisse_fiwi_mico4sys/fiwi_mico4sys001/metagenomics/processed/04_D/04_FastUniq
+
+DATABASES=/storage/scratch/users/rj23k073/human
+
+SAMPLE=1_1
+
+
+humann --input "$INDIR"/"$SAMPLE".R1.dedup.fastq.gz,"$INDIR"/"$SAMPLE".R2.dedup.fastq.gz \
+      --output out/"$SAMPLE" \
       --threads 16 \
       --nucleotide-database /path/to/chocophlan \
       --protein-database /path/to/uniref90
