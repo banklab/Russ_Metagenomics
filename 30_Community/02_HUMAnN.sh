aebc62965d7555b64df03b1c1d3829eb3f154217
russjasp <61029511+russjasp@users.noreply.github.com>
2025-11-26 11:47:29 +0100

    Update 02_HUMAnN.sh

--- 30_Community/02_HUMAnN.sh
+++ 30_Community/02_HUMAnN.sh
@@ -1,6 +1,6 @@
 
 conda activate human
-
+## also has 'seqtk'
 
 INDIR=/storage/workspaces/vetsuisse_fiwi_mico4sys/fiwi_mico4sys001/metagenomics/processed/04_D/04_FastUniq
 
@@ -8,9 +8,13 @@ DATABASES=/storage/workspaces/vetsuisse_fiwi_mico4sys/fiwi_mico4sys001/metagenom
 
 SAMPLE=1_1
 
+## interleave R1 & R2
+seqtk mergepe $INDIR/${SAMPLE}.R1.dedup.fastq.gz $INDIR/${SAMPLE}.R2.dedup.fastq.gz | gzip > "$SAMPLE".interleaved.fastq.gz
 
-humann --input "$INDIR"/"$SAMPLE".R1.dedup.fastq.gz,"$INDIR"/"$SAMPLE".R2.dedup.fastq.gz \
+
+## human on interleaved reads
+humann --input "$SAMPLE".interleaved.fastq.gz \
       --output out/"$SAMPLE" \
       --threads 16 \
-      --nucleotide-database /path/to/chocophlan \
-      --protein-database /path/to/uniref90
+      --nucleotide-database "$DATABASES"/chocophlan \
+      --protein-database "$DATABASES"/uniref90
