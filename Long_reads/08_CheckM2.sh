8e6ef4407659c4fed7f71933c9230532e8a14137
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-02 18:07:15 +0100

    Update 08_CheckM2.sh

--- Long_reads/08_CheckM2.sh
+++ Long_reads/08_CheckM2.sh
@@ -10,13 +10,13 @@ conda activate checkm2_env
 #SBATCH --mail-type=FAIL,END
 #SBATCH --output=slurm-%x.%j.out
 
-for i in *_DASTool_bins
+for i in /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/07_DAS_Tool/*_DASTool_bins
 do
  SAMPLE=$(basename $i _DASTool_bins)
 
  echo $SAMPLE
 
- checkm2 predict --input /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/07_DAS_Tool/$i \
+ checkm2 predict --input $i \
   --output_directory /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_CheckM2/"$SAMPLE" \
  -x fa \
  -t 16
