058ff40d2a07d484ea847690829eef5f2ac0d23e
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-02 12:20:04 +0100

    Create 07b_DAS_Tool.sh

---
+++ Long_reads/07b_DAS_Tool.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --mem=20000M
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=6:00:00
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8
+
+for asm in /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/03_assembly/*.asm.p_ctg.filtered.fa
+do
+
+SAMPLE=$(basename "$asm" .asm.p_ctg.gfa)
+
+DAS_Tool -c $asm \
+        -i /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/07_DAS_Tool/Contigs_to_Bins/"$SAMPLE"_MetaBAT2.tsv,/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/07_DAS_Tool/Contigs_to_Bins/"$SAMPLE"_MaxBin2.tsv,/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/07_DAS_Tool/Contigs_to_Bins/"$SAMPLE"_SemiBin2.tsv  \
+        -l MetaBAT2,MaxBin2,SemiBin2 \
+         -o $SAMPLE \
+         --search_engine diamond \
+         --write_bins \
+         -t 4
+done
