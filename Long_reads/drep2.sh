0319f132be21d9f2efebf0b4f6309fc7f604ad8d
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-15 07:27:54 +0100

    Create drep2.sh

---
+++ Long_reads/drep2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --mem=120000M
+#SBATCH --time=5:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8   
+dRep dereplicate 05_drep_LR_bins_and_SR_bins \
+-g 04_input_LR_bins_and_SR_bins/*fa \
+-comp 70 -con 10 \
+--S_algorithm ANImf \
+--gen_warnings -ms 10000 -pa 0.8 -sa 0.95 -nc 0.30 \
+--genomeInfo genomeInformation_Step1_bins_and_short_read_bins.csv \
+-p 24 \
+-comW 1 -conW 5 \
+--extra_weight_table Extra_Weight_Table2.txt 
