1e6ce1167ecf471b664cfb191d76fcd16daa5035
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-15 11:47:19 +0100

    Update 11_scaffold_to_bin_file.sh

--- Long_reads/11_scaffold_to_bin_file.sh
+++ Long_reads/11_scaffold_to_bin_file.sh
@@ -1,7 +1,7 @@
 
 
-module load Python/3.10.8-GCCcore-12.2.0
+module load Python/3.9.5-GCCcore-10.3.0
 
-cd 08_dRep/05_drep_LR_bins_and_SR_bins/dereplicated_genomes/
+chmod +x parse_stb.py 
 
-parse_stb.py --reverse -f *fa -o DEER_v2.stb
+./parse_stb.py --reverse -f /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/05_drep_LR_bins_and_SR_bins/dereplicated_genomes/*fa -o DEER_v2.stb
