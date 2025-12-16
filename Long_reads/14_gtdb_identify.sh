e4e81d947b4f6d7acc49a7ee51a7598e82b70c54
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-16 07:02:07 +0100

    Create 14_gtdb_identify.sh

---
+++ Long_reads/14_gtdb_identify.sh
@@ -0,0 +1,18 @@
+actually run on ubelix
+
+#!/bin/bash
+#SBATCH --mem=20000M
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+
+module load Python/3.10.4-GCCcore-11.3.0
+module load prodigal/2.6.3-GCCcore-10.3.0
+module load HMMER/3.3.2-gompi-2021a
+export GTDBTK_DATA_PATH=/storage/workspaces/vetsuisse_fiwi_mico4sys/fiwi_mico4sys001/metagenomics/programs/GTDB/release220/
+
+gtdbtk identify --genome_dir /storage/scratch/users/rj23k073/04_Deer/IBU/taxonomy/genomes --out_dir . --extension fa --cpus 4
