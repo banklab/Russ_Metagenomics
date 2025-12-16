7c9bd173718547ddbc6cd63578e9d39a01f45e5b
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-16 10:12:12 +0100

    Update 14b_gtfb_align.sh

--- Long_reads/14b_gtfb_align.sh
+++ Long_reads/14b_gtfb_align.sh
@@ -13,4 +13,4 @@ module load prodigal/2.6.3-GCCcore-10.3.0
 module load HMMER/3.3.2-gompi-2021a
 export GTDBTK_DATA_PATH=/storage/workspaces/vetsuisse_fiwi_mico4sys/fiwi_mico4sys001/metagenomics/programs/GTDB/release220/
 
-gtdbtk align --identify_dir identify --out_dir align --cpus 1
+gtdbtk align --identify_dir . --out_dir align --cpus 1
