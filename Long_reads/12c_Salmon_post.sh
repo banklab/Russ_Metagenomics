1a58d78a253769a291e44a6f786cc6050ba7e2b5
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-17 10:11:17 +0100

    Update 12c_Salmon_post.sh

--- Long_reads/12c_Salmon_post.sh
+++ Long_reads/12c_Salmon_post.sh
@@ -7,4 +7,13 @@ mkdir -p quant_files
 
 mv *quant.counts quant_files/
 
+#!/bin/bash
+#SBATCH --mem=20000M
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=03:00:00
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
 python 04_split_salmon_out_into_bins.py quant_files /rs_scratch/users/rj23k073/04_Deer/IBU/taxonomy/genomes DEER_v2.fa > bin_abundance_table_DEER_v2.tab
