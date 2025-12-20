81d7c0fa2f4dc3feadc82df60783a08af1945f76
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-20 15:02:41 +0100

    Update 13d_gtdb_format.R

--- Long_reads/13d_gtdb_format.R
+++ Long_reads/13d_gtdb_format.R
@@ -32,18 +32,18 @@ DF$Species <- gsub(".*s__","",DF$classification)
 
 DF2$bin <- gsub("_deer","",DF2$user_genome) ## extract taxonomy information for Archaea
 
-DF$bin <- gsub("_deer","",DF$user_genome) ## extract taxonomy information for Bacteria
+DF2$bin <- gsub("_deer","",DF2$user_genome) ## extract taxonomy information for Bacteria
 
-DF$Method <- "SR"
+DF2$Method <- "SR"
 
-DF[grepl("metabat|maxbin|semibin",DF$bin),"Method"] <- "LR"
-DF[grepl("hybrid",DF$bin),"Method"] <- "Hy"
+DF2[grepl("metabat|maxbin|semibin",DF2$bin),"Method"] <- "LR"
+DF2[grepl("hybrid",DF2$bin),"Method"] <- "Hy"
 
-DF$bin <- gsub(".*metabat","Me",DF$bin)
-DF$bin <- gsub(".*maxbin","Ma",DF$bin)
-DF$bin <- gsub(".*semibin","Se",DF$bin)
+DF2$bin <- gsub(".*metabat","Me",DF2$bin)
+DF2$bin <- gsub(".*maxbin","Ma",DF2$bin)
+DF2$bin <- gsub(".*semibin","Se",DF2$bin)
 
-DF$bin <- paste0(DF$Method,"_",DF$bin)
+DF2$bin <- paste0(DF2$Method,"_",DF2$bin)
   
 
 
