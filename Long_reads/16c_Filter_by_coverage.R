74dbba53bfa687da3fdc48a1f3a5f9502338325c
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-23 09:39:21 +0100

    Update 16c_Filter_by_coverage.R

--- Long_reads/16c_Filter_by_coverage.R
+++ Long_reads/16c_Filter_by_coverage.R
@@ -7,9 +7,12 @@ snp_list <- list.files(pattern=(paste0("_Env",EnvA,"xEnv",EnvB,"_Filter_snps")))
 
 for(i in 1:length(snp_list)){
 
+  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/11_InStrain/Filtered_Sites")
   snp_df <- data.frame(fread(snp_list[i],header=T, stringsAsFactors=F))
 
+  species2 <- gsub("_Env.*","",snp_list[i])
 
+  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/mosdepth")
 
 
   }
