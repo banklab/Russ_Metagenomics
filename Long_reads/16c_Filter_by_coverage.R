ee3f0ee1cb2dec9c22081a72932dd6a20e11f669
russjasp <61029511+russjasp@users.noreply.github.com>
2025-12-23 09:54:56 +0100

    Update 16c_Filter_by_coverage.R

--- Long_reads/16c_Filter_by_coverage.R
+++ Long_reads/16c_Filter_by_coverage.R
@@ -1,19 +1,31 @@
 
 library(data.table)
 
+EnvA <- 8
+EnvB <- 10
+
+SNP_filter <- 20e3 ## only using species with at least x number of USEABLE snps (snps that can go into cmh test)
+
 
 setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/11_InStrain/Filtered_Sites")
 snp_list <- list.files(pattern=(paste0("_Env",EnvA,"xEnv",EnvB,"_Filter_snps")))
+snp_count <- as.numeric(gsub(".*snps|\\.csv","",snp_list))
+snp_list2 <- snp_list[snp_list>=SNP_filter]
 
-for(i in 1:length(snp_list)){
+
+for(i in 1:length(snp_list2)){
 
   setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/11_InStrain/Filtered_Sites")
-  snp_df <- data.frame(fread(snp_list[i],header=T, stringsAsFactors=F))
+  snp_df <- data.frame(fread(snp_list2[i],header=T, stringsAsFactors=F))
 
-  species2 <- gsub("_Env.*","",snp_list[i])
+  species2 <- gsub("_Env.*","",snp_list2[i])
 
-  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/mosdepth")
+  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/mosdepth/BED")
 
+  
+
+
+  
 
   }
 
