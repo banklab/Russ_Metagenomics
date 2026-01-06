13dd1c5b01737d4bc583967178cdd415f1fe2451
russjasp <61029511+russjasp@users.noreply.github.com>
2026-01-06 09:00:57 +0100

    Update 03_extract_reads_from_matching_species.sh

--- Long_reads/Cryptobacteroides/03_extract_reads_from_matching_species.sh
+++ Long_reads/Cryptobacteroides/03_extract_reads_from_matching_species.sh
@@ -3,16 +3,6 @@ module load SAMtools/1.13-GCC-10.3.0
 
 awk '{print $1 "\t0\t1000000000"}' species_scaffolds_with_transposase.txt > species_scaffolds_with_transposase.bed
 
-
-samtools view -b \
-  -L species_scaffolds_with_transposase.bed \
-  /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/09_Alignment/1_10_LR.sorted.bam \
-  > 1_10_transposase.bam
-
-
-samtools sort 1_10_transposase.bam -o 1_10_transposase.sorted.bam
-samtools index 1_10_transposase.sorted.bam
-
 #!/bin/bash
 #SBATCH --mem=60000M
 #SBATCH --nodes=1
