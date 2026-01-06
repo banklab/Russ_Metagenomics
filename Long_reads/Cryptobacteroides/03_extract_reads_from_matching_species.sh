288789e827f5e689c9fb336f63cc06872f157b75
russjasp <61029511+russjasp@users.noreply.github.com>
2026-01-06 08:52:09 +0100

    Update 03_extract_reads_from_matching_species.sh

--- Long_reads/Cryptobacteroides/03_extract_reads_from_matching_species.sh
+++ Long_reads/Cryptobacteroides/03_extract_reads_from_matching_species.sh
@@ -14,11 +14,19 @@ samtools sort 1_10_transposase.bam -o 1_10_transposase.sorted.bam
 samtools index 1_10_transposase.sorted.bam
 
 
-samtools view -b \
-  -L species_scaffolds_with_transposase.bed \
-  /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/09_Alignment/1_10_LR.sorted.bam \
-  > 1_10_transposase.bam
+for f in /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/09_Alignment/*sorted.bam; do
+    
+    sample=$(basename "$f" _LR.sorted.bam)
+
+    samtools view -b \
+      -L species_scaffolds_with_transposase.bed \
+      "$f" \
+      > "$sample"_transposase.bam
+    
+    samtools sort "$sample"_transposase.bam -o "$sample"_transposase.sorted.bam
+    samtools index "$sample"_transposase.sorted.bam
+
+done
 
 
-samtools sort 1_10_transposase.bam -o 1_10_transposase.sorted.bam
-samtools index 1_10_transposase.sorted.bam
+
