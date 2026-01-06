a4abd52454f090f6493521668e196e44cd20e38e
russjasp <61029511+russjasp@users.noreply.github.com>
2026-01-06 10:24:26 +0100

    Create 05_map_to_ONE_representative_genome.sh

---
+++ Long_reads/Cryptobacteroides/05_map_to_ONE_representative_genome.sh
@@ -0,0 +1,7 @@
+
+## representative Cryptobacteroides
+## hybrid_semibin_2_8_bin.23 (analog to 2_10_bin.9)
+
+module load Bowtie2/2.4.4-GCC-10.3.0
+bowtie2-build hybrid_semibin_2_8_bin.23.fa hybrid_semibin_2_8_bin.23
+
