12096cc83d56295e5e1144491e80d0341ddfab9b
russjasp <61029511+russjasp@users.noreply.github.com>
2026-01-06 09:07:26 +0100

    Update 04_convert_to_fastq.sh

--- Long_reads/Cryptobacteroides/04_convert_to_fastq.sh
+++ Long_reads/Cryptobacteroides/04_convert_to_fastq.sh
@@ -1,9 +1,16 @@
 
+module load SAMtools/1.13-GCC-10.3.0
+
+for f in *transposase.sorted.bam; do
+    
+    sample=$(basename "$f" _transposase.sorted.bam)
 
 samtools fastq \
-  -1 1_8_transposase_R1.fq \
-  -2 1_8_transposase_R2.fq \
+  -1 "$f"_transposase_R1.fq \
+  -2 "$f"_transposase_R2.fq \
   -0 /dev/null \
   -s /dev/null \
   -n \
-  1_8_transposase.sorted.bam
+  "$f"
+
+done
