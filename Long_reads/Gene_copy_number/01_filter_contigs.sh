05d94b992e5bb8d72bfad52fc1fe84459943b9a5
russjasp <61029511+russjasp@users.noreply.github.com>
2026-01-09 12:10:43 +0100

    Update 01_filter_contigs.sh

--- Long_reads/Gene_copy_number/01_filter_contigs.sh
+++ Long_reads/Gene_copy_number/01_filter_contigs.sh
@@ -2,8 +2,8 @@ module load SeqKit/2.6.1
 
 for i in *deer.asm_contigs.fasta
 do
-sample=
-#seqkit seq -m 1000 $i > 
-
+sample=${i%_deer.asm_contigs.fasta} 
+seqkit seq -m 1000 $i > "$sample".filtered.contigs.fasta
+#echo "$sample".filtered.contigs.fasta
 done
 
