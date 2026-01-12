e48791838bcb3b6b1b82673c80aa312bc44caab5
russjasp <61029511+russjasp@users.noreply.github.com>
2026-01-12 08:28:47 +0100

    Create 04_match_clusted_aa_to_nucl.R

---
+++ Long_reads/Gene_copy_number/04_match_clusted_aa_to_nucl.R
@@ -0,0 +1,25 @@
+
+
+library(Biostrings)
+
+## fasta clustered at 50% identity (amino acids)
+aa_rep <- "genes50_rep_seq.fasta"
+## prodigal output (nucleotide)
+nt_all <- "genes_nt.fna"
+
+## output file - a fasta clustered at 50% identity, but it's the dna now instead of amino acids
+out_fasta <- "genes50_rep_seq_nt.fna"
+
+aa_seqs <- readAAStringSet(aa_rep)
+
+# prodigal id's
+rep_ids <- sub(" .*", "", names(aa_seqs))
+
+# all nucleotide seqs
+nt_seqs <- readDNAStringSet(nt_all)
+
+keep <- names(nt_seqs) %in% rep_ids
+nt_rep <- nt_seqs[keep]
+
+# now nucleotides at 50% clustering
+writeXStringSet(nt_rep, out_fasta)
