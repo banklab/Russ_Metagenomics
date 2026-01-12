5b31755477645991aaf39fa9d448490c5b5863ed
russjasp <61029511+russjasp@users.noreply.github.com>
2026-01-12 10:01:05 +0100

    Update 04_match_clusted_aa_to_nucl.R

--- Long_reads/Gene_copy_number/04_match_clusted_aa_to_nucl.R
+++ Long_reads/Gene_copy_number/04_match_clusted_aa_to_nucl.R
@@ -1,27 +1,4 @@
 
+# fasta clustered at 50% identity (amino acids)
+# now I want the corresponding DNA sequences from the clusters (amino acids)
 
-library(Biostrings)
-
-## fasta clustered at 50% identity (amino acids)
-aa_rep <- "genes50_rep_seq.fasta"
-## prodigal output (nucleotide)
-nt_all <- "nucl_from_contigs.fna"
-
-## output file - a fasta clustered at 50% identity, but it's the dna now instead of amino acids
-out_fasta <- "genes50_rep_seq_DNA.fna"
-
-aa_seqs <- readAAStringSet(aa_rep)
-
-# prodigal id's
-rep_ids <- sub(" .*", "", names(aa_seqs))
-
-nt_ids <- sub(" .*", "", names(nt_seqs))
-
-# all nucleotide seqs
-nt_seqs <- readDNAStringSet(nt_all)
-
-keep <- nt_ids %in% rep_ids
-nt_rep <- nt_seqs[keep]
-
-# now nucleotides at 50% clustering
-writeXStringSet(nt_rep, out_fasta)
