c8fa2ff81d4ce45ddd9936deda51dbebb5b96987
russjasp <61029511+russjasp@users.noreply.github.com>
2026-02-17 10:02:25 +0100

    Update 01_growth_rate_format.R

--- Long_reads/Further/01_growth_rate_format.R
+++ Long_reads/Further/01_growth_rate_format.R
@@ -11,6 +11,13 @@ for(i in 1:length(instrain_dirs)){
 
   df <- fread(paste0(sample1,"_LR_InStrain_genome_info.tsv"), header=T, stringsAsFactors=F, sep="\t")
 
-  df$SAMPLE <- 
+  df$SAMPLE <- sample1
+  df$Deer <- as.numeric(gsub("_.*","",sample1))
+  df$Env <- as.numeric(gsub(".*_","",sample1))
 
- 
+  if(i==1){ df2 <- df } else { df2 <- rbind(df2,df) }
+
+}
+
+setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/11_InStrain")
+write.csv(df2, "Genome_Info_DEER_v2.csv", row.names=F)
