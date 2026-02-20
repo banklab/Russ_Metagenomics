91a3aef7f3d9524866c7e731354cb44fe38a96c3
russjasp <61029511+russjasp@users.noreply.github.com>
2026-02-20 08:58:27 +0100

    Update outlier_frequency.R

--- Long_reads/Diversity/outlier_frequency.R
+++ Long_reads/Diversity/outlier_frequency.R
@@ -52,8 +52,8 @@ for(s in 1:length(cmh_files2)){
   cmh_data2$OUTLIER <- cmh_data2$qvalue <= q_min_thresh
   
   
-  if(sum(cmh_data2$OUTLIER)<1){next}
-
+  if(sum(cmh_data2$OUTLIER)<1){message("no outs\n");next}
+ 
   cmh_outs <- cmh_data2[cmh_data2$OUTLIER,]
 
 
@@ -65,15 +65,51 @@ for(s in 1:length(cmh_files2)){
   for(i in 1:sum(cmh_data2$OUTLIER)){
 
     outlier_site <- cmh_outs[i,]
-    
-    snp_df[snp_df$format_scaffold==outlier_site$Scaffold & snp_df$POS==outlier_site$POS,]
 
+    ## find flanking snps
+    outlier_scaffold <- snp_df[snp_df$format_scaffold==outlier_site$Scaffold,]
+    positions <- unique(outlier_scaffold$POS)
+    outlier_index <- which(positions==outlier_site$POS)
 
+    flanking_positions <-  positions[c(outlier_index-4):c(outlier_index+4)]
+
+    for(j in 1:length(flanking_positions)){
+
+        outlier_snps <- outlier_scaffold[outlier_scaffold$POS==flanking_positions[j],]
+        
+        alleles <- outlier_snps[,c("A","C","G","T")]
+          
+        alleles2 <- names(alleles)[order(colSums(alleles), decreasing=T)]
+      
+        major.llele <- alleles2[1]
+        minor.llele <- alleles2[2]
+        #minor.llele2 <- alleles2[3]
+        #minor.llele3 <- alleles2[4]
     
+        outlier_snps$Major.Freq <- outlier_snps[,major.llele] / outlier_snps$DP
+        outlier_snps$Minor.Freq <- outlier_snps[,minor.llele] / outlier_snps$DP
+        outlier_snps$Major.Allele <- major.llele
+        outlier_snps$Minor.Allele <- minor.llele
+
+    if(j==1){ outlier_snps2 <- outlier_snps } else { outlier_snps2 <- rbind(outlier_snps2,outlier_snps) }
+        
     }
 
+    outlier_snps2$OUTLIER <- FALSE
+    outlier_snps2[outlier_snps2$format_scaffold==outlier_site$Scaffold & outlier_snps2$POS==outlier_site$POS,"OUTLIER"] <- TRUE
 
-  
+    outlier_snps2$logq <- NA
+    outlier_snps2[outlier_snps2$format_scaffold==outlier_site$Scaffold & outlier_snps2$POS==outlier_site$POS,"logq"] <- outlier_site$logq
+      
+    }
+
+    if(i==1){ outlier_full <- outlier_snps2 } else { outlier_full <- rbind(outlier_full,outlier_snps2) }
+
+    filename <- paste0(SPECIES1,"_outlier_frequencies_DEER_v2.csv")
+    
+    setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/Diversity")
+    write.csv(outlier_full, filename, row.names=F)
+    
 }
 
 
