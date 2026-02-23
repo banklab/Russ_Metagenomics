f6aa9a0516e0abd44fadc7fb2f35dd5e96966e7d
russjasp <61029511+russjasp@users.noreply.github.com>
2026-02-23 13:53:26 +0100

    Update calc_fst_add_gene_intergenic.R

--- Long_reads/Diversity/calc_fst_add_gene_intergenic.R
+++ Long_reads/Diversity/calc_fst_add_gene_intergenic.R
@@ -1,5 +1,24 @@
 
 library(data.table)
 
+setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/Diversity")
 fst_files <- list.files(pattern="_FST_deer_v2.csv")
 
+for(i in 1:length(fst_files)){
+
+  
+  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/Diversity")
+  fst_species <- fread(fst_files[i], header=T, stringsAsFactors = F)
+
+  fst_species[!is.na(fst_species$Fst) & fst_species$Fst<0,"Fst"] <- 0
+
+  species_label <- gsub("_FST.*","",fst_files[i])
+
+  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/Diversity")
+
+  
+  
+}
+
+
+ 
