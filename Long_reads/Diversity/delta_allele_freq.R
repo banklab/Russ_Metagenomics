3bdf40ae295de0cc4705f1dc4fd697a3f5fd1560
russjasp <61029511+russjasp@users.noreply.github.com>
2026-03-03 08:44:31 +0100

    Update delta_allele_freq.R

--- Long_reads/Diversity/delta_allele_freq.R
+++ Long_reads/Diversity/delta_allele_freq.R
@@ -1,8 +1,54 @@
 
 library(data.table)
 
-EnvA <- 8
-EnvB <- 10
+
+delta.function <- function(one.site){ 
+
+    snps <- snp_df[snp_df$ID==one.site,]
+
+    ## snps with only 1 environment
+    if(length(unique(snps$Env))<2){return(NULL)}
+
+    counts <- colSums(snps[,c("A","C","G","T")], na.rm=T)
+
+    alleles <- sort(counts, decreasing=T)
+    
+    order.data <- snps[,c("Deer","Env","DP",names(alleles))]
+
+    order.data[,names(alleles)] <- order.data[,names(alleles)] / order.data[,"DP"]
+
+    colnames(order.data)[4:7]<- c("Major.freq","Minor.freq","Minor2","Minor3")
+
+    wide.data <- reshape(order.data[order.data$Env %in% c(8,10), ],
+                idvar = "Deer",
+                timevar = "Env",
+                direction = "wide")
+
+    num.alleles <- sum(alleles>0)
+    wide.data$num.alleles <- num.alleles
+    
+    wide.data$Major.Diff <- wide.data$Major.freq.10 - wide.data$Major.freq.8
+    wide.data$Minor.Diff <- wide.data$Minor.freq.10 - wide.data$Minor.freq.8
+
+    wide.data$Minor2.Diff <- NA
+    wide.data$Minor3.Diff <- NA
+    
+    if(num.alleles>2){ ## at least 3 alleles
+        wide.data$Minor2.Diff <- wide.data$Minor2.10 - wide.data$Minor2.8
+        if(num.alleles>3){ ## all 4 alleles
+            wide.data$Minor3.Diff <- wide.data$Minor3.10 - wide.data$Minor3.8
+        }
+    }
+  
+    wide.data$ID <- one.site
+
+    return(wide.data)
+}
+
+
+# Env hardcoded actually
+#EnvA <- 8
+#EnvB <- 10
 
 
 cmh_species <- c('Hy_Me_2_8_bin.181','Hy_Me_2_8_bin.89','Hy_Me_4_8_bin.143','Hy_Se_2_10_bin.105','Hy_Se_2_8_bin.23','Hy_Se_2_8_bin.24','Hy_Se_4_8_bin.39','Hy_Se_6_8_bin.0','Hy_Se_6_8_bin.55','Hy_Se_6_9_bin.30','LR_Me_4_8_bin.270','LR_Me_6_9_bin.102_sub','LR_Me_6_9_bin.166','LR_Se_2_10_bin.36','LR_Se_6_10_bin.83')
@@ -18,58 +64,19 @@ for(s in 1:length(cmh_species)){
   snp_df <- data.frame(fread(list.files(pattern=paste0(SPECIES1,"_Env")), header=T, stringsAsFactors = F))
  
   snp_df$format_scaffold <- gsub("_asm_.*","",snp_df$Scaffold)
-  
-  for(i in 1:sum(cmh_data2$OUTLIER)){
 
-    outlier_site <- cmh_outs[i,]
 
-    ## find flanking snps
-    outlier_scaffold <- snp_df[snp_df$format_scaffold==outlier_site$Scaffold,]
-    positions <- unique(outlier_scaffold$POS)
-    outlier_index <- which(positions==outlier_site$POS)
+  delta_list <- lapply(unique(snp_df$ID)[1:1588], FUN=delta.function)
+  delta_results <- as.data.frame(do.call(rbind, delta_list))
+  rownames(delta_results) <- NULL
 
-    flanking_positions <-  positions[c(outlier_index-4):c(outlier_index+4)]
+  delta_results$Mean.DP <- rowMeans(delta_results[,c("DP.8","DP.10")], na.rm=T)
 
-    for(j in 1:length(flanking_positions)){
+  filename2 <- paste0(SPECIES1,"_delta.csv")
 
-        outlier_snps <- outlier_scaffold[outlier_scaffold$POS==flanking_positions[j],]
-        
-        alleles <- outlier_snps[,c("A","C","G","T")]
-          
-        alleles2 <- names(alleles)[order(colSums(alleles), decreasing=T)]
-      
-        major.llele <- alleles2[1]
-        minor.llele <- alleles2[2]
-        #minor.llele2 <- alleles2[3]
-        #minor.llele3 <- alleles2[4]
-    
-        outlier_snps$Major.Freq <- outlier_snps[,major.llele] / outlier_snps$DP
-        outlier_snps$Minor.Freq <- outlier_snps[,minor.llele] / outlier_snps$DP
-        outlier_snps$Major.Allele <- major.llele
-        outlier_snps$Minor.Allele <- minor.llele
+  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DiversityDELTA")
+  write.csv(delta_results, filename2, row.names=F)
 
-        outlier_snps$Window_position <- j
-        
-        
-    if(j==1){ outlier_snps2 <- outlier_snps } else { outlier_snps2 <- rbind(outlier_snps2,outlier_snps) }
-        
-    }
-
-    outlier_snps2$Window <- i
-    outlier_snps2$OUTLIER <- FALSE
-    outlier_snps2[outlier_snps2$format_scaffold==outlier_site$Scaffold & outlier_snps2$POS==outlier_site$POS,"OUTLIER"] <- TRUE
-
-    outlier_snps2$logq <- NA
-    outlier_snps2[outlier_snps2$format_scaffold==outlier_site$Scaffold & outlier_snps2$POS==outlier_site$POS,"logq"] <- outlier_site$logq
-
-    if(i==1){ outlier_full <- outlier_snps2 } else { outlier_full <- rbind(outlier_full,outlier_snps2) }
-
-    }
-
-  
-    filename <- paste0(SPECIES1,"_outlier_frequencies_DEER_v2.csv")
-    
-    setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/Diversity")
-    write.csv(outlier_full, filename, row.names=F)
-    
 }
+
+    
