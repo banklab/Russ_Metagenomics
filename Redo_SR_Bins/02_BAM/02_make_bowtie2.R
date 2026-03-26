b2297d6a50f755f64b146788e71274ce39c83f7f
russjasp <61029511+russjasp@users.noreply.github.com>
2026-03-26 13:10:05 +0100

    Create 02_make_bowtie2.R

---
+++ Redo_SR_Bins/02_BAM/02_make_bowtie2.R
@@ -0,0 +1,49 @@
+
+READ_DIR="/storage/scratch/users/rj23k073/04_DEER/04_FastUniq"
+ASM_DIR="/storage/scratch/users/rj23k073/04_DEER/06_Assembly"
+OUTPUT_DIR="/storage/scratch/users/rj23k073/04_DEER/07_BAM"
+
+DATASET="deer"
+
+
+setwd(ASM_DIR)
+
+Read_list <- gsub(paste0("_",DATASET,".asm"),"",list.files(pattern = paste0("_",DATASET,".asm")))
+
+setwd(OUTPUT_DIR)
+
+
+for(i in 1:length(Read_list)){
+  
+  reads1 <- Read_list[i]
+  
+  sh_name <- paste0(reads1,"_bowtie2.sh")
+
+  ## align reads against assemblies
+  code_block <- paste0("bowtie2 -p 20 -x ",ASM_DIR,"/",reads1,"_",DATASET,".asm/scaffolds_filtered -1 ",READ_DIR,"/",reads1,".R1.dedup.fastq -2 ",READ_DIR,"/",reads1,".R2.dedup.fastq -S ",OUTPUT_DIR,"/",reads1,".sam")
+
+  ## convert sam to bam
+  code_block2 <- paste0("samtools view -Sb ",OUTPUT_DIR,"/",reads1,".sam > ",OUTPUT_DIR,"/",reads1,".bam")
+
+  ## remove sam file
+  code_block3 <- paste0("rm ",OUTPUT_DIR,"/",reads1,".sam")
+
+  code_block4 <- paste0("echo Finished ",reads1)
+  
+  write ("#!/bin/bash", sh_name)
+  write ("#SBATCH --mem=4000M", sh_name, append = TRUE) ## adjust MEM, time, cpus etc
+  write ("#SBATCH --nodes=1", sh_name, append = TRUE)
+  write ("#SBATCH --ntasks=1", sh_name, append = TRUE)
+  write ("#SBATCH --cpus-per-task=20", sh_name, append = TRUE)
+  write ("#SBATCH --time=01:30:00", sh_name, append = TRUE)
+  write ("#SBATCH --mail-user=<russell.jasper@unibe.ch>", sh_name, append = TRUE) ## adjust email for job finish/fail etc
+  write ("#SBATCH --mail-type=FAIL,END", sh_name, append = TRUE)
+  write ("#SBATCH --output=slurm-%x.%j.out", sh_name, append = TRUE)
+  write ("module load SAMtools/1.13-GCC-10.3.0", sh_name, append = TRUE) ## load samtools/bowtie2 according to server specifications
+  write ("module load Bowtie2/2.4.4-GCC-10.3.0", sh_name, append = TRUE)
+  write (code_block, sh_name, append = TRUE)
+  write (code_block2, sh_name, append = TRUE)
+  write (code_block3, sh_name, append = TRUE)
+  write (code_block4, sh_name, append = TRUE)
+
+}
