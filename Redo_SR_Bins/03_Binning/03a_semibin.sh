6c4e614b61faf69f944b7d55f14f72e20605e854
russjasp <61029511+russjasp@users.noreply.github.com>
2026-03-27 08:49:42 +0100

    Update 03a_semibin.sh

--- Redo_SR_Bins/03_Binning/03a_semibin.sh
+++ Redo_SR_Bins/03_Binning/03a_semibin.sh
@@ -15,14 +15,14 @@ ASM_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/01_Ass
 BAM_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/02_BAM
 
 ## parallelize by deer here
-for i in "$ASM_DIR"/2*.asm.p_ctg.filtered.fa
+for i in "$ASM_DIR"/7*_deer.asm/*_scaffolds_filtered_NoNorm.fasta
 do
     ASM1=$(basename "$i") 
-    ASM=${ASM1%.asm.p_ctg.filtered.fa} 
-
+    ASM="${ASM1%%_scaffolds*}"  
+    
     echo "Input Assembly: $ASM"
 
-    SemiBin2 generate_sequence_features_single -i $i -b $BAM_DIR/${ASM}/*.bam -o out/"$ASM" -t 1
+    SemiBin2 generate_sequence_features_single -i $i -b $BAM_DIR/"${ASM}"_deer/*.bam -o out/"$ASM" -t 1
 
    
     echo -e "Done\n"
