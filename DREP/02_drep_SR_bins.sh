9baf185a1eaa1db7e6955c3bad90f696d7493655
russjasp <61029511+russjasp@users.noreply.github.com>
2026-03-27 12:58:45 +0100

    Update 02_drep_SR_bins.sh

--- DREP/02_drep_SR_bins.sh
+++ DREP/02_drep_SR_bins.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+#SBATCH --mem=100000M
+#SBATCH --time=16:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8
+dRep dereplicate /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/99_short_read_bins/drep80 -g /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/99_short_read_bins/RAW/*fa -comp 70 -con 10 --S_algorithm ANImf --gen_warnings -ms 10000 -pa 0.65 -sa 0.80 -nc 0.30 --genomeInfo raw_Short_read_bins_CheckM2.csv -p 10
