c0b5f66f1bb6457bdf0269324b1f12520a2666ae
russjasp <61029511+russjasp@users.noreply.github.com>
2026-03-30 11:27:27 +0200

    Update 03b_semibin.sh

--- Redo_SR_Bins/03_Binning/03b_semibin.sh
+++ Redo_SR_Bins/03_Binning/03b_semibin.sh
@@ -4,8 +4,8 @@ conda activate semibin
 #SBATCH --mem=50000M
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --time=120:00:00
 #SBATCH --mail-user=<russell.jasper@unibe.ch>
 #SBATCH --mail-type=FAIL,END
 #SBATCH --output=slurm-%x.%j.out
@@ -15,16 +15,16 @@ ASM_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/01_Ass
 BAM_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/02_BAM
 
 ## parallelize by deer here
-for i in "$ASM_DIR"/7*_deer.asm/*_scaffolds_filtered_NoNorm.fasta
+for i in "$ASM_DIR"/2*_deer.asm/*_scaffolds_filtered_NoNorm.fasta
 do
     ASM1=$(basename "$i") 
     ASM="${ASM1%%_scaffolds*}"  
     
     echo "Input Assembly: $ASM"
 
-    SemiBin2 train_self --data out/"$ASM"/data.csv --data-split out/"$ASM"/data_split.csv -o out/"$ASM" -t 8 --epochs 20 --engine cpu
+    SemiBin2 train_self --data out/"$ASM"/data.csv --data-split out/"$ASM"/data_split.csv -o out/"$ASM" -t 16 --epochs 20 --engine cpu
 
-    SemiBin2 bin -i $i --data out/"$ASM"/data.csv --model out/"$ASM"/model.pt -o out/"$ASM" -t 8 --compression none
+    SemiBin2 bin -i $i --data out/"$ASM"/data.csv --model out/"$ASM"/model.pt -o out/"$ASM" -t 16 --compression none
    
     echo -e "Done\n"
 
