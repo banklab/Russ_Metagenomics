d41cc6546b6b85567dc276f1426563253d7b8093
russjasp <61029511+russjasp@users.noreply.github.com>
2026-03-31 08:47:32 +0200

    Create 06_Index.sh

---
+++ DREP/06_Index.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mem==40000M
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=3:00:00
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8 
+
+module load Bowtie2/2.4.4-GCC-10.3.0
+
+for i in *fa
+do
+basename="${i%.fa}"
+bowtie2-build $i $basename
+done
