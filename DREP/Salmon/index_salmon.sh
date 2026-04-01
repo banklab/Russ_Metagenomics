94a06068573008e85a830681aa90c62c8f8a1352
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-01 07:37:14 +0200

    Create index_salmon.sh

---
+++ DREP/Salmon/index_salmon.sh
@@ -0,0 +1,24 @@
+actually run on ubelix
+
+#!/bin/bash
+#SBATCH --mem=60000M
+#SBATCH --time=24:00:00
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=pibu_el8
+
+for i in DEER_drep*fa
+do
+
+tmp="${i%.fa}"
+#drep_val="${tmp##*drep}"
+
+salmon index -p 4 -t $i -i "$tmp"_salmon_index
+
+echo "done $tmp"
+
+done
