fb4b73fccb7bf80bc5c679ae47989c6ac3852f47
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-01 10:59:22 +0200

    Update 02_salmon.sh

--- DREP/Salmon/02_salmon.sh
+++ DREP/Salmon/02_salmon.sh
@@ -1,1 +1,22 @@
 
+REF_DIR=/storage/scratch/users/rj23k073/04_Deer/DREP/salmon/DEER_drep80_salmon_index
+
+OUTDIR=/storage/scratch/users/rj23k073/04_Deer/DREP/salmon/DEER_drep80_salmon_out
+
+mkdir -p $OUTDIR
+
+
+INDIR=/storage/workspaces/vetsuisse_fiwi_mico4sys/fiwi_mico4sys001/metagenomics/processed/04_D/03_Trim
+
+
+for Read1 in "$INDIR"/*_R1.trim.fastq.gz
+do
+
+name=$(basename "$Read1")
+ID="${name%%_R1*}"
+    
+salmon quant -i "$REF_DIR" --libType IU -1 "$Read1" -2 "${Read1/_R1./_R2.}" -o "$OUTDIR"/"$ID".quant --meta -p 1
+
+echo "done $ID"
+
+done
