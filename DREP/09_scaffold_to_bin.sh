109f32717f4203e1ee74e863dfe1d85d8b85003a
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-02 13:56:28 +0200

    Update 09_scaffold_to_bin.sh

--- DREP/09_scaffold_to_bin.sh
+++ DREP/09_scaffold_to_bin.sh
@@ -4,11 +4,25 @@ module load Python/3.9.5-GCCcore-10.3.0
 
 cd /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/11_InStrain
 
+
+## 80's drep
 for i in /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/05_8*_drep_LR_bins_and_SR_bins
 do
 
-drep2=$(basename "$i") 
-drep1="${drep2%%_drep_*}"  
-drep="${drep1%%05_}"  
+drep1=$(basename "$i") 
+drep=$(echo "$drep1" | cut -d'_' -f2)
 
-./parse_stb.py --reverse -f /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/05_80_drep_LR_bins_and_SR_bins/dereplicated_genomes/*fa -o DEER_v2.stb
+./parse_stb.py --reverse -f /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/05_"$drep"_drep_LR_bins_and_SR_bins/dereplicated_genomes/*fa -o /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DREP/01_References/DEER_drep"$drep".stb
+
+done
+
+## 90's drep
+for i in /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/05_9*_drep_LR_bins_and_SR_bins
+do
+
+drep1=$(basename "$i") 
+drep=$(echo "$drep1" | cut -d'_' -f2)
+
+./parse_stb.py --reverse -f /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/05_"$drep"_drep_LR_bins_and_SR_bins/dereplicated_genomes/*fa -o /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DREP/01_References/DEER_drep"$drep".stb
+
+done
