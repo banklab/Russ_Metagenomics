5c7112c011f64891447ae79e4251d5898997b8ee
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-02 07:43:32 +0200

    Create 07B_Alignment.sh

---
+++ DREP/07B_Alignment.sh
@@ -0,0 +1,42 @@
+## skip some environments
+
+#!/bin/bash
+#SBATCH --mem=40000M
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=120:00:00
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8 
+
+module load Bowtie2/2.4.4-GCC-10.3.0
+module load SAMtools/1.13-GCC-10.3.0
+
+
+REF=DEER_drep80
+
+REF_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DREP/01_References
+READS_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/04_FastUniq
+OUT_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DREP/02_Alignment
+
+
+drep_val="${REF##*_}"  
+
+
+for read in "$READS_DIR"/*R1.dedup.fastq.gz
+do
+
+ID1=$(basename "$read")
+ID="${ID1%%.*}"
+
+BAM="$OUT_DIR/${ID}_${drep_val}.bam"
+
+echo "Mapping $ID to $REF"
+
+bowtie2 -p 12 -x "$REF_DIR"/"$REF" -1 "$read" -2 "${read/.R1./.R2.}" \
+        | samtools sort -@4 -o "$BAM"
+    samtools index "$BAM"
+
+done
