28661da86dd2ff81b0ab74166cac1afeebe78fc3
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-07 14:41:55 +0200

    Create 03_drep1.sh

---
+++ Redo_SR_Bins/05_drep/03_drep1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --mem=200000M
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8   
+dRep dereplicate 03_drep_long_read_bins_and_hybrid_bins \
+-g 01_input/*fa \
+-comp 70 -con 10 \
+--S_algorithm ANImf \
+--gen_warnings -ms 10000 -pa 0.8 -sa 0.95 -nc 0.30 \
+--genomeInfo genomeInformation.csv \
+-p 24
