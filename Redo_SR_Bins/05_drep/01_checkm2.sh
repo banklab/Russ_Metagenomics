d1ca58828ac3a83e21570509a9275fb66abb8ed4
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-07 14:29:12 +0200

    Create 01_checkm2.sh

---
+++ Redo_SR_Bins/05_drep/01_checkm2.sh
@@ -0,0 +1,13 @@
+conda activate checkm2
+
+#!/bin/bash
+#SBATCH --mem=100000M
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8
+checkm2 predict --input 01_input --output_directory 02_CheckM2 -x fa -t 16
