fc81ab8f994d1af6adf582cea6380d03cb1c68e9
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-07 08:14:31 +0200

    Update 03c_semibin.sh

--- Redo_SR_Bins/03_Binning/03c_semibin.sh
+++ Redo_SR_Bins/03_Binning/03c_semibin.sh
@@ -3,14 +3,14 @@
 
 dest=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/03_Binning/03_SemiBin2/bins
 
-cd /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/06_Binning/03_SemiBin2/out
+cd /data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/03_Binning/03_SemiBin2/out
 
 for d in */; do
     dir=${d%/} 
     for f in "$dir"/output_bins/SemiBin_*.fa; do
         base=$(basename "$f")
         new="semibin_${dir}_bin.${base#SemiBin_}"
-        #cp "$f" "$dest/$new"
+        cp "$f" "$dest/$new"
 	echo $f
 	echo $new
     done
