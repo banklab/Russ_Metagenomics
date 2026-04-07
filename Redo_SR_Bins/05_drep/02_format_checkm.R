70dcf6fb9ae6f5d724a75f63ce13d3af8cdb902c
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-07 14:42:05 +0200

    Update 02_format_checkm.R

--- Redo_SR_Bins/05_drep/02_format_checkm.R
+++ Redo_SR_Bins/05_drep/02_format_checkm.R
@@ -8,4 +8,4 @@ colnames(checkm)[1:3] <- c("genome","completeness","contamination")
 checkm$genome <- paste0(checkm$genome,".fa")
 
 setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/05_drep2")
-write.csv(checkm, "genomeInformation_round1.csv", row.names=F)
+write.csv(checkm, "genomeInformation.csv", row.names=F)
