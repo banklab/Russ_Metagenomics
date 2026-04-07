190319e0331685055a92a4435b24c07cdf41b559
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-07 09:35:25 +0200

    Update 02_DAS_Tools.sh

--- Redo_SR_Bins/04_DAS_Tools/02_DAS_Tools.sh
+++ Redo_SR_Bins/04_DAS_Tools/02_DAS_Tools.sh
@@ -11,12 +11,16 @@ conda activate das_tools
 #SBATCH --output=slurm-%x.%j.out
 #SBATCH --partition=pibu_el8
 
-for asm in /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/03_assembly/*.asm.p_ctg.filtered.fa
+
+## loop over only the (LR) assemblies 
+for asm in /data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/04_DAS_Tool/Contigs_to_Bins/*_MetaBAT2.tsv
 do
 
-SAMPLE=$(basename "$asm" .asm.p_ctg.filtered.fa)
+SAMPLE=$(basename "$asm" _MetaBAT2.tsv)
 
-DAS_Tool -c $asm \
+SR_ASM="$SAMPLE"_deer.asm/"$SAMPLE"_scaffolds_filtered_NoNorm.fasta
+
+DAS_Tool -c /data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/01_Assembly/"$SR_ASM" \
         -i /data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/04_DAS_Tool/Contigs_to_Bins/"$SAMPLE"_MetaBAT2.tsv,/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/04_DAS_Tool/Contigs_to_Bins/"$SAMPLE"_MaxBin2.tsv,/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/04_DAS_Tool/Contigs_to_Bins/"$SAMPLE"_SemiBin2.tsv  \
         -l MetaBAT2,MaxBin2,SemiBin2 \
          -o $SAMPLE \
