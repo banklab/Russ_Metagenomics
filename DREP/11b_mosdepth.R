69302b0e3aa2217d57a0439a27e54c405f93de51
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-08 09:41:13 +0200

    Create 11b_mosdepth.R

---
+++ DREP/11b_mosdepth.R
@@ -0,0 +1,43 @@
+
+library(data.table)
+setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DREP/08_mosdepth/BED")
+cov_files <- list.files(pattern="base.bed$")
+
+## parallel by environment
+cov_files <- cov_files[grepl("_1_drep",cov_files)]
+
+for(i in 1:length(cov_files)){
+
+cov_df <- fread(cov_files[i],stringsAsFactors=F)
+
+colnames(cov_df) <- c("Scaffold","start","end","coverage")
+
+cov_df$Sample <- gsub("_drep.*","",cov_files[i])
+cov_df$Deer <- as.numeric(gsub("_.*","",cov_files[i]))
+cov_df$Env <- as.numeric(gsub(".*_","",cov_df$Sample[1]))
+
+cov_df$bin <- gsub(".*asm_","",cov_df$Scaffold)
+
+cov_df$Method <- "SR"
+
+Drep_val <- gsub(".*_drep|\\.per.*","",cov_files[i])
+
+cov_df$DREP <- as.numeric(Drep_val)
+  
+ if(i==1){ cov_df2 <- cov_df } else { cov_df2 <- rbind(cov_df2,cov_df) }
+
+}
+
+ENV <- unique(cov_df2$Env)
+
+write.csv(cov_df2, paste0("ENV",ENV,"_temp_mosdepth.csv), row.names=F)
+
+
+##
+
+sp_list <- unique(cov_df2$bin)
+
+for(ii in 1:length(sp_list)){
+sp_cov <- cov_df2[cov_df2$bin==sp_list[ii],]
+write.csv(sp_cov, paste0(sp_list[ii],"_cover.csv"), row.names=F)
+}
