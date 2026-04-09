7b59c0af4ff78f72b153b3ec84fb3b3933cafe07
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-09 14:35:47 +0200

    Create 10_InStrain.sh

---
+++ DREP/10_InStrain.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --mem=100000M
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=72:00:00
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8 
+module load Python/3.9.5-GCCcore-10.3.0
+module load SAMtools
+
+drep=80
+
+
+BAM_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DREP/02_Alignment
+OUTPUT_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DREP/04_InStrain/DEER_drep"$drep"
+
+mkdir -p $OUTPUT_DIR
+
+REF_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/DREP/01_References
+REF=DEER_drep"$drep".fa
+STB=DEER_drep"$drep".stb  
+Prodigal=DEER_drep"$drep".genes.fna
+
+
+## SUBSET DEER ##
+for bam in "$BAM_DIR"/1_*drep"$drep".bam
+do
+
+ID1=$(basename "$bam")
+ID="${ID1%_*}"
+
+SNP_OUT="$OUTPUT_DIR"/"$ID"_drep"$drep"
+
+echo "Call snps $ID on $REF"
+
+inStrain profile $bam "$REF_DIR"/"$REF" -o $SNP_OUT -s "$REF_DIR"/"$STB" -g "$REF_DIR"/"$Prodigal" -p 4 --database_mode
+
+done
