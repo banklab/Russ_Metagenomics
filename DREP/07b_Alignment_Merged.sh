2489e0308eda5ee917825b18b7af6786aa125e2f
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-09 10:55:44 +0200

    Create 07b_Alignment_Merged.sh

---
+++ DREP/07b_Alignment_Merged.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --mem=2000M
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8
+module load SAMtools/1.13-GCC-10.3.0
+
+envA=8
+envB=10
+
+DREP=80
+
+for i in {1..7}
+do
+echo "deer $i"
+
+samtools merge - \
+  "$i"_"$envA"_drep"$DREP".bam \
+  "$i"_"$envB"_drep"$DREP".bam \
+| samtools sort -o deer"$i"_env"$envA"_env"$envB"_drep"$DREP".bam -
+
+samtools index deer"$i"_env"$envA"_env"$envB"_drep"$DREP".bam
+
+done
