1e21e6bc6e45440f312d08c706ac7d2e588e8145
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-09 07:40:01 +0200

    Update 11c_mosdepth.R

--- DREP/11c_mosdepth.R
+++ DREP/11c_mosdepth.R
@@ -8,5 +8,5 @@ sp_list <- unique(cov_df2$bin)
 
 for(ii in 1:length(sp_list)){
 sp_cov <- cov_df2[cov_df2$bin==sp_list[ii],]
-write.csv(sp_cov, paste0(sp_list[ii],"_ENV",ENV,"coverage.csv"), row.names=F)
+write.csv(sp_cov, paste0(sp_list[ii],"_ENV",ENV,"_coverage.csv"), row.names=F)
 }
