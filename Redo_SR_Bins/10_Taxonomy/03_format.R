34f28b6bd23805fea7023771bbbf86e8be2f231f
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-27 10:00:50 +0200

    Create 03_format.R

---
+++ Redo_SR_Bins/10_Taxonomy/03_format.R
@@ -0,0 +1,45 @@
+
+
+library(data.table)
+
+
+setwd("")
+DF <- data.frame(fread("gtdbtk.bac120.summary.tsv", header=T, stringsAsFactors = F, sep="\t"))
+DF2 <- data.frame(fread("gtdbtk.ar53.summary.tsv", header=T, stringsAsFactors = F, sep="\t"))
+
+DF$bin <- gsub("metabat_|maxbin_|semibin_","",DF$user_genome) ## extract taxonomy information for Bacteria
+
+DF$Method <- "SR"
+
+DF$bin <- paste0(DF$Method,"_",DF$bin)
+  
+
+DF$Domain <- gsub("d__|;p__.*","",DF$classification)
+DF$Phylum <- gsub(".*p__|;c__.*","",DF$classification)
+DF$Class <- gsub(".*c__|;o__.*","",DF$classification)
+DF$Order <- gsub(".*o__|;f__.*","",DF$classification)
+DF$Family <- gsub(".*f__|;g__.*","",DF$classification)
+DF$Genus <- gsub(".*g__|;s__.*","",DF$classification)
+DF$Species <- gsub(".*s__","",DF$classification)
+
+
+DF2$bin <- gsub("metabat_|maxbin_|semibin_","",DF2$user_genome) ## extract taxonomy information for Archaea
+
+DF2$Method <- "SR"
+
+DF2$bin <- paste0(DF2$Method,"_",DF2$bin)
+  
+
+
+DF2$Domain <- gsub("d__|;p__.*","",DF2$classification)
+DF2$Phylum <- gsub(".*p__|;c__.*","",DF2$classification)
+DF2$Class <- gsub(".*c__|;o__.*","",DF2$classification)
+DF2$Order <- gsub(".*o__|;f__.*","",DF2$classification)
+DF2$Family <- gsub(".*f__|;g__.*","",DF2$classification)
+DF2$Genus <- gsub(".*g__|;s__.*","",DF2$classification)
+DF2$Species <- gsub(".*s__","",DF2$classification)
+
+
+full_tax <- rbind(DF,DF2)
+
+write.csv(full_tax, "DEER_SR_Taxonomy.csv", row.names = F)
