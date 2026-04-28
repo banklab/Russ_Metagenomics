d6e592fb9823dc079a51b93af41106b185bc040b
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-28 08:25:16 +0200

    Create 02_format.R

---
+++ Redo_SR_Bins/07_InStrain/02_format.R
@@ -0,0 +1,46 @@
+library(data.table)
+
+for(ENV in 1:10){
+for(DEER in 1:7){
+
+  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/08_InStrain/RAW")
+ snv_file <- data.frame(fread(paste0(DEER,"_",ENV,"_SR_InStrain_SNVs.tsv"), header=T, stringsAsFactors = F))
+  
+  snv_file$bin <- gsub(".*asm_","",snv_file$scaffold) ## make variable for species (bin)
+
+  snv_file$Method <- "SR"
+
+
+  snv_file$bin <- paste0(snv_file$Method,"_",snv_file$bin)
+  
+  if(sum(grep("scaffold",snv_file$bin))>0){stop("ERROR")}
+  
+  snv_file$Deer <- DEER ## record deer ## change as study system requires
+  
+  snv_file$Env <- ENV ## record sampling environment ## change as study system requires
+  
+  snv_file$Scaffold <- gsub(".*NODE_|_length_.*","",snv_file$scaffold) ## record scaffold
+  
+  snv_file$POS <- snv_file$position ## record position
+  
+  snv_file$DP <- snv_file$position_coverage ## record coverage
+
+  colnames(snv_file)[1] <- "scaffold2"
+  
+  snv_file2 <- snv_file[,c(20,22:26,4:19,1,21)]
+
+  snv_file2$ID <- paste0(snv_file2$Scaffold,"_",snv_file2$POS) ## make snp ID with scaffold and position
+
+  snv_file2$Sp.ID <- paste0(snv_file2$bin,"_sc",snv_file2$Scaffold,"_pos",snv_file2$POS) ## species-specific snp ID
+
+  snv_file2$Sp.ID.deer <- paste0(snv_file2$bin,"_sc",snv_file2$Scaffold,"_pos",snv_file2$POS,"_d",snv_file2$Deer) ## species-deer-specific snp ID
+
+  snv_file2$Original <- TRUE ## these are original snps called by InStrain (later I add in sites that were fixed reference, ie, not original)
+  
+ 
+
+  setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/08_InStrain/FORMAT")
+  write.csv(snv_file2, paste0(DEER,"_",ENV,"_SR_InStrain_SNVs_format.csv"), row.names = F)
+  
+}}
+
