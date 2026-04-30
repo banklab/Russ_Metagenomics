fa3c86e899cc205aeee8eccb78404d96d1008331
russjasp <61029511+russjasp@users.noreply.github.com>
2026-04-30 13:36:42 +0200

    Update 01_CoverM.sh

--- LR_vs_SR/01_CoverM.sh
+++ LR_vs_SR/01_CoverM.sh
@@ -1,5 +1,26 @@
+
+## LR
+
+BAM_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/09_Alignment
+
+for bam in "$BAM_DIR"/2_*LR.sorted.bam
+do
+
+
+BASE=$(basename "$bam")
+
+ID="${BASE%%_LR.sorted.bam}"
+
+OUTPUT="$ID".LR.abundance.tsv
+
+
 coverm genome \
-  -b aln.bam \
-  --genome-fasta-directory genomes/ \
+  -b $bam \
+  --genome-fasta-files /data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/08_dRep/05_drep_LR_bins_and_SR_bins/dereplicated_genomes/*fa \
   -m relative_abundance mean \
-  -o abundance.tsv
+  -o $OUTPUT
+
+done
+
+--genome-fasta-files *.fa
+## SR
