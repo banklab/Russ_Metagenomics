72a03dc3c331a750add39962be3bfdc88f3a60ca
russjasp <61029511+russjasp@users.noreply.github.com>
2026-05-01 09:25:49 +0200

    Update 01b_CoverM.sh

--- LR_vs_SR/01b_CoverM.sh
+++ LR_vs_SR/01b_CoverM.sh
@@ -24,4 +24,17 @@ if(i==1){ cover_full <- cov_df2 } else { cover_full <- rbind(cover_full,cov_df2)
 cover_full$Deer <- as.numeric(gsub("_.*","",cover_full$Sample))
 cover_full$Env <- as.numeric(gsub(".*_","",cover_full$Sample))
 
+cover_full$bin <- gsub("deer_","",cover_full$Genome)
+
+cover_full$Method <- "SR"
+
+cover_full[grepl("metabat|maxbin|semibin",cover_full$bin),"Method"] <- "LR"
+cover_full[grepl("hybrid",cover_full$bin),"Method"] <- "Hy"
+
+cover_full$bin <- gsub(".*metabat","Me",cover_full$bin)
+cover_full$bin <- gsub(".*maxbin","Ma",cover_full$bin)
+cover_full$bin <- gsub(".*semibin","Se",cover_full$bin)
+
+cover_full$bin <- paste0(cover_full$Method,"_",cover_full$bin) 
+
 write.csv(cover_full, "DEER_v2_CoverM.csv", row.names=F)
