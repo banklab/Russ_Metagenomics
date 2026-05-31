ceace8fb89151faece8c569685b8aa595ff36718
russjasp <61029511+russjasp@users.noreply.github.com>
2026-05-31 08:18:17 +0200

    Create 05_filter_CSVs.R

---
+++ Long_reads/Extend_Envs/05_filter_CSVs.R
@@ -0,0 +1,44 @@
+
+
+library(data.table)
+
+
+SNP_filter <- 18e3
+
+setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/16_CMH/04_CMH")
+cmh_files <- list.files(pattern=".csv")
+snp_count <- as.numeric(gsub(".*tests|_CMH.*","",cmh_files))
+cmh_files2 <- cmh_files[snp_count>=SNP_filter]
+
+
+for(i in 1:length(cmh_files2)){
+
+  cmh_df <- fread(cmh_files2[i], header=T, stringsAsFactors=F)
+
+  species2 <- gsub("_Env.*", "", cmh_files2[i])
+
+  EnvA <- as.numeric(gsub(".*_Env|xEnv.*", "", cmh_files2[i]))
+  EnvB <- as.numeric(gsub(".*xEnv|_tests.*", "", cmh_files2[i]))
+
+  num_test <- as.numeric(gsub(".*_tests|_CMH.*", "", cmh_files2[i]))
+
+  cmh_df$bin <- species2
+  cmh_df$EnvA <- EnvA
+  cmh_df$EnvB <- EnvB
+  cmh_df$tests <- num_test
+
+
+  if(i==1){ cmh_all <- cmh_df } else { cmh_all<- rbind(cmh_all,cmh_df) }
+
+  }
+
+
+cmh_all$rep <- paste0(cmh_all$bin,"_",cmh_all$EnvA,"_",cmh_all$EnvB)
+
+
+write.csv(cmh_all, "CMH_environments.csv", row.names=F)
+
+
+
+
+                              
