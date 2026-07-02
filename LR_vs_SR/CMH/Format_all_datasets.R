5b147d3ac7f7afd0447ef91683d94977820da0d6
russjasp <61029511+russjasp@users.noreply.github.com>
2026-07-02 11:36:19 +0200

    Update Format_all_datasets.R

--- LR_vs_SR/CMH/Format_all_datasets.R
+++ LR_vs_SR/CMH/Format_all_datasets.R
@@ -1,3 +1,12 @@
 
+library(data.table)
+
 setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/16_CMH/04_CMH")
+SNP_filter <- 20e3 
+
+setwd("//data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/12_CMH/03_Filtered_by_Coverage")
+cmh_list <- list.files(pattern="_Coverage_Filter_snps")
+snp_count <- as.numeric(gsub(".*snps|\\.csv","",cmh_list))
+
+cmh_list2 <- cmh_list[snp_count>=SNP_filter]
 
