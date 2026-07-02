760c8af2c289fe8f2fce55cbcd59564b5d04fb38
russjasp <61029511+russjasp@users.noreply.github.com>
2026-07-02 11:42:28 +0200

    Update Format_all_datasets.R

--- LR_vs_SR/CMH/Format_all_datasets.R
+++ LR_vs_SR/CMH/Format_all_datasets.R
@@ -1,12 +1,29 @@
 
 library(data.table)
 
-setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/16_CMH/04_CMH")
+
 SNP_filter <- 20e3 
 
-setwd("//data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/12_CMH/03_Filtered_by_Coverage")
-cmh_list <- list.files(pattern="_Coverage_Filter_snps")
-snp_count <- as.numeric(gsub(".*snps|\\.csv","",cmh_list))
+
+setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/16_CMH/04_CMH")
+cmh_list <- list.files(pattern="_CMH.csv")
+snp_count <- as.numeric(gsub(".*_tests|.*snps|_CMH.csv","",cmh_list))
 
 cmh_list2 <- cmh_list[snp_count>=SNP_filter]
+
+for(i in 1:length(cmh_list2)){
+
+cmh_df <- fread(cmh_list2[i], header=T, stringsAsFactors=F)
+  
+snp_count1 <- as.numeric(gsub(".*_tests|.*snps|_CMH.csv","",cmh_list2[i]))
+
+cmh_df$tests <- snp_count1
+
+cmh_df$Data <- "LR28SR70"
+
+if(i==1){ cmh_df2 <- cmh_df } else { cmh_df2 <- rbind(cmh_df2,cmh_df) }
+  
+  }
+
+
 
