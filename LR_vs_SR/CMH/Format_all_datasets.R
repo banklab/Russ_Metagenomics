fa7f661acadcd968f09e5103ff421b9bb461676f
russjasp <61029511+russjasp@users.noreply.github.com>
2026-07-02 12:02:36 +0200

    Update Format_all_datasets.R

--- LR_vs_SR/CMH/Format_all_datasets.R
+++ LR_vs_SR/CMH/Format_all_datasets.R
@@ -14,6 +14,8 @@ cmh_list2 <- cmh_list[snp_count>=SNP_filter]
 for(i in 1:length(cmh_list2)){
 
 cmh_df <- fread(cmh_list2[i], header=T, stringsAsFactors=F)
+
+cmh_df$bin <- gsub("_Env.*","",cmh_list2[i])
   
 snp_count1 <- as.numeric(gsub(".*_tests|.*snps|_CMH.csv","",cmh_list2[i]))
 
@@ -24,6 +26,81 @@ cmh_df$Data <- "LR28SR70"
 if(i==1){ cmh_df2 <- cmh_df } else { cmh_df2 <- rbind(cmh_df2,cmh_df) }
   
   }
+
+
+
+setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/Outliers_LR_ONLY/03_CMH_on_SR_alignments/04_CMH")
+cmhA_list <- list.files(pattern="_CMH.csv")
+snp_count <- as.numeric(gsub(".*_tests|.*snps|_LR28_CMH.csv","",cmhA_list))
+
+cmhA_list2 <- cmhA_list[snp_count>=SNP_filter]
+
+for(i in 1:length(cmhA_list2)){
+
+cmhA_df <- fread(cmhA_list2[i], header=T, stringsAsFactors=F)
+
+  
+cmhA_df$bin <- gsub("_Env.*","",cmhA_list2[i])
+
+snp_count11 <- as.numeric(gsub(".*_tests|.*snps|_LR28_CMH.csv","",cmhA_list2[i]))
+
+cmhA_df$tests <- snp_count11
+
+cmhA_df$Data <- "LR28"
+
+if(i==1){ cmhA_df2 <- cmhA_df } else { cmhA_df2 <- rbind(cmhA_df2,cmhA_df) }
+  
+  }
+
+
+
+
+setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/16_CMH/04_CMH")
+cmhB_list <- list.files(pattern="_CMH.csv")
+snp_count <- as.numeric(gsub(".*_tests|.*snps|_CMH.csv","",cmhB_list))
+
+cmhB_list2 <- cmhB_list[snp_count>=SNP_filter]
+
+for(i in 1:length(cmhB_list2)){
+
+cmhB_df <- fread(cmhB_list2[i], header=T, stringsAsFactors=F)
+
+  cmhB_df$bin <- gsub("_Env.*","",cmhB_list2[i])
+
+snp_count111 <- as.numeric(gsub(".*_tests|.*snps|_CMH.csv","",cmhB_list2[i]))
+
+cmhB_df$tests <- snp_count111
+
+cmhB_df$Data <- "SR70"
+
+if(i==1){ cmhB_df2 <- cmhB_df } else { cmhB_df2 <- rbind(cmhB_df2,cmhB_df) }
+  
+  }
+
+
+
+setwd("/data/projects/p898_Deer_RAS_metagenomics/04_Deer/REDO_SR_Binning/12_CMH/04_CMH")
+cmhC_list <- list.files(pattern="_CMH.csv")
+snp_count <- as.numeric(gsub(".*_tests|.*snps|_SR28_CMH.csv","",cmhC_list))
+
+cmhC_list2 <- cmhC_list[snp_count>=SNP_filter]
+
+for(i in 1:length(cmhC_list2)){
+
+cmhC_df <- fread(cmhC_list2[i], header=T, stringsAsFactors=F)
+
+    cmhC_df$bin <- gsub("_Env.*","",cmhC_list2[i])
+
+snp_count1111 <- as.numeric(gsub(".*_tests|.*snps|_SR28_CMH.csv","",cmhC_list2[i]))
+
+cmhC_df$tests <- snp_count1111
+
+cmhC_df$Data <- "SR28"
+
+if(i==1){ cmhC_df2 <- cmhC_df } else { cmhC_df2 <- rbind(cmhC_df2,cmhC_df) }
+  
+  }
+
 
 
 
