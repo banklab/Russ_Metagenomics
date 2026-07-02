8f854bc4cee60128406110b8331053a6e99c863f
russjasp <61029511+russjasp@users.noreply.github.com>
2026-07-02 08:39:07 +0200

    Create dRep_methods_95.sh

---
+++ LR_vs_SR/dRep/dRep_methods_95.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --mem=100000M
+#SBATCH --time=16:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8
+dRep dereplicate 02_drep95 -g 01_genomes/*fa -comp 70 -con 10 --S_algorithm ANImf --gen_warnings -ms 10000 -pa 0.80 -sa 0.95 -nc 0.30 --genomeInfo all_good_copy_genomes_CheckM.csv -p 10
