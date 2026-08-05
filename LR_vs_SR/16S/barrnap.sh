5d430b4542ac54c412350fb459f794fa3be479f2
russjasp <61029511+russjasp@users.noreply.github.com>
2026-08-05 11:47:08 +0200

    Update barrnap.sh

--- LR_vs_SR/16S/barrnap.sh
+++ LR_vs_SR/16S/barrnap.sh
@@ -1,9 +1,23 @@
+#!/bin/bash
+#SBATCH --mem=8000M
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-user=<russell.jasper@unibe.ch>
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=slurm-%x.%j.out
+#SBATCH --partition=pibu_el8
 
-## predict 16S from MAGs
 
-
-for f in TEST/*.fa
+for f in 01_LR28_genomes/*.fa
 do
     base=$(basename "$f" .fa)
-    barrnap --kingdom bac "$f" > "02_Barrnap/${base}.gff"
+    barrnap --kingdom bac "$f" > "02_Barrnap/LR_28_${base}.gff"
+done
+
+for f in 01_SR28_genomes/*.fa
+do
+    base=$(basename "$f" .fa)
+    barrnap --kingdom bac "$f" > "02_Barrnap/SR_28_${base}.gff"
 done
