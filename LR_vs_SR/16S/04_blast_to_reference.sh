8219a6c9f6ea64457b03f0253bdfb23e0f54a6aa
russjasp <61029511+russjasp@users.noreply.github.com>
2026-08-08 07:52:24 +0200

    Update 04_blast_to_reference.sh

--- LR_vs_SR/16S/04_blast_to_reference.sh
+++ LR_vs_SR/16S/04_blast_to_reference.sh
@@ -1,5 +1,7 @@
 
-SILVA_SSURef_NR99.nocontam.masked.trimmed.NR99.fasta
-https://zenodo.org/records/18627380
+
+# 16S reference 99% derep
+# SILVA_SSURef_NR99.nocontam.masked.trimmed.NR99.fasta
+# https://zenodo.org/records/18627380
 
 
