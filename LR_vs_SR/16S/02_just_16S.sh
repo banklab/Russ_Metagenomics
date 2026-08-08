77efc184ec9105b918338cc9806620cdeb455cab
russjasp <61029511+russjasp@users.noreply.github.com>
2026-08-08 17:06:46 +0200

    Update 02_just_16S.sh

--- LR_vs_SR/16S/02_just_16S.sh
+++ LR_vs_SR/16S/02_just_16S.sh
@@ -7,3 +7,14 @@ awk -F'\t' '$3=="rRNA" && $9 ~ /Name=16S_rRNA/ {print}' \
 $f > ../04_rRNA_only_16S/${base}_16S.gff
 
 done
+
+
+for f in SR28_contigs*gff
+do
+base=$(basename "$f" .gff)
+
+
+awk -F'\t' '$3=="rRNA" && $9 ~ /Name=16S_rRNA/ {print}' \
+$f > ../04_rRNA_only_16S/${base}_16S.gff
+
+done
