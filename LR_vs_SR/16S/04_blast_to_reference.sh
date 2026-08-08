36ed442359b7459ab2dae0a89c8e0577f53c0985
russjasp <61029511+russjasp@users.noreply.github.com>
2026-08-08 08:02:16 +0200

    Update 04_blast_to_reference.sh

--- LR_vs_SR/16S/04_blast_to_reference.sh
+++ LR_vs_SR/16S/04_blast_to_reference.sh
@@ -4,4 +4,8 @@
 # SILVA_SSURef_NR99.nocontam.masked.trimmed.NR99.fasta
 # https://zenodo.org/records/18627380
 
+# taxonomy of sequences
+# silva-138-99-tax.qza
+# https://docs.qiime2.org/2024.10/data-resources/
+
 
