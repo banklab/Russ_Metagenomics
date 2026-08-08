4b166edcef651c110610a68df476e387b3023e6c
russjasp <61029511+russjasp@users.noreply.github.com>
2026-08-08 07:51:24 +0200

    Create 03_extract_sequences.sh

---
+++ LR_vs_SR/16S/03_extract_sequences.sh
@@ -0,0 +1,22 @@
+
+
+module load BEDTools/2.30.0-GCC-10.3.0
+
+ASM_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/LONG_READS/03_assembly
+OUT_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/METHODS/16S/05_extracted_sequences
+
+
+for f in *16S.gff
+do
+
+ID="${f#LR28_contigs_}"
+ID="${ID%_16S.gff}"
+
+bedtools getfasta \
+    -fi "$ASM_DIR"/"$ID".asm.p_ctg.fa \
+    -bed $f \
+    -s \
+    -name \
+    > "$OUT_DIR"/LR28_"$ID"_contigs_16S.fa
+
+done
