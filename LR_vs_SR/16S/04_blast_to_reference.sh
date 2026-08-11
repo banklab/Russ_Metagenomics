c3751e0d6fa2ab3ac67a5ce19a314710d1d7b574
russjasp <61029511+russjasp@users.noreply.github.com>
2026-08-11 10:56:54 +0200

    Update 04_blast_to_reference.sh

--- LR_vs_SR/16S/04_blast_to_reference.sh
+++ LR_vs_SR/16S/04_blast_to_reference.sh
@@ -38,4 +38,31 @@ blastn \
     -evalue 1e-20
 done
 
+
+
+
+
+module load BLAST+/2.15.0-gompi-2021a
+
+DATABASE=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/METHODS/16S/SILVA_database
+OUT_DIR=/data/projects/p898_Deer_RAS_metagenomics/04_Deer/METHODS/16S/06_blast_hits
+
+for f in SR28_*_scaffolds_16S.fa
+
+do
+
+base=$(basename "$f" _contigs_16S.fa)
+blastfile="$OUT_DIR"/"$base"_vs_16S_SILVA.out
+
+blastn \
+    -query $f \
+    -db "$DATABASE"/SILVA_SSURef_NR99 \
+    -out "$blastfile" \
+   -outfmt '6 qseqid sseqid pident length qcovs evalue bitscore stitle' \
+    -max_target_seqs 10 \
+    -evalue 1e-20
+done
+
+    
+
     
