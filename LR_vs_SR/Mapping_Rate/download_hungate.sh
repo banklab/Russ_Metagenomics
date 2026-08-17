084073f38a40ca6b6f0aaeb344e27b3ed738380a
russjasp <61029511+russjasp@users.noreply.github.com>
2026-08-17 12:22:19 +0200

    Update download_hungate.sh

--- LR_vs_SR/Mapping_Rate/download_hungate.sh
+++ LR_vs_SR/Mapping_Rate/download_hungate.sh
@@ -1,5 +1,7 @@
+# 400 genomes from hungate400.tsv
+https://vetgenomics.github.io/metagenomics/hungate1000/
+
+
 conda activate ncbi-datasets
 
-datasets download genome accession PRJNA471733 \
-    --include genome \
-    --filename hungate1000.zip
+datasets download genome accession --inputfile hungate1000_400_accessions.txt --include genome --filename hungate1000_400.zip
